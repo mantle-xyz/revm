09528a66ad6512c4f1277e6f3bf12e412a997595
PinelliaC <wylbzc4928@gmail.com>
2025-03-11 18:12:42 +0800

    refactor: streamline BVM_ETH transfer logic in handler_register

--- crates/revm/src/optimism/handler_register.rs
+++ crates/revm/src/optimism/handler_register.rs
@@ -202,12 +202,12 @@ pub fn deduct_caller<SPEC: Spec, EXT, DB: Database>(
             optimism::bvm_eth::mint_bvm_eth(context, U256::from(eth_value))?;
             optimism::bvm_eth::touch_bvm_eth_contract(context);
         }
-        // transfer BVM_ETH
-        if let Some(eth_tx_value) = context.evm.inner.env.tx.optimism.eth_tx_value {
-            if eth_tx_value != 0 {
-                optimism::bvm_eth::transfer_bvm_eth(context, U256::from(eth_tx_value))?;
-                optimism::bvm_eth::touch_bvm_eth_contract(context);
-            }
+    }
+    // transfer BVM_ETH
+    if let Some(eth_tx_value) = context.evm.inner.env.tx.optimism.eth_tx_value {
+        if eth_tx_value != 0 {
+            optimism::bvm_eth::transfer_bvm_eth(context, U256::from(eth_tx_value))?;
+            optimism::bvm_eth::touch_bvm_eth_contract(context);
         }
     }
 
